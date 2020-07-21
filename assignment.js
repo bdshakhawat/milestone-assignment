21e8e3c81e73ba7abd86949d9a6e3a6822f71d9f
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-07-20 19:28:54 -0700

    new commit

---
+++ assignment.js
@@ -0,0 +1,10 @@
+// Assignment topic 1.
+var feetToMile = 50000;
+function lengthConvertion(feetToMile){
+    return feetToMile/5280;
+}
+var result = lengthConvertion(feetToMile);
+console.log(result);
+// End of Topic 1.
+//  Assignment topic 2
+
