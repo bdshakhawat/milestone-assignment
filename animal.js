b48e439bc74c62acb0542d6b2568ff91bed637b6
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-07-21 23:53:04 -0700

    another commit

---
+++ animal.js
@@ -0,0 +1,19 @@
+var building = 2;
+var brickCalculator = 0;
+if ( building<= 10){
+    brickCalculator = building*15*1000;
+}
+else if ( building <=20){
+    var first10 = 10*15*1000;
+    var remaining = building - 10;
+    var second10 = remaining*12*1000;
+    brickCalculator = first10 + second10;
+}
+else {
+    first10 = 10*15*1000;
+    second10 = 10*12*1000;
+    var remaining = building - 20;
+    var restPart = remaining*10*1000;
+    brickCalculator = first10 + second10 + restPart;
+}
+console.log(brickCalculator);
